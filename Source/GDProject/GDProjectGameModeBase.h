8ff225a3e81818e3f2c8a35ee7174d29c7ed56d9
Mattia De Prisco <mattia.dipi@gmail.com>
2021-05-31 23:07:50 +0200

    Empty project

---
+++ Source/GDProject/GDProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GDProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GDPROJECT_API AGDProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
