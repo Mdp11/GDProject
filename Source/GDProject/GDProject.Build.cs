3037fe27629f24ba2568b704fa8afc6571cf6538
Mattia De Prisco <mattia.dipi@gmail.com>
2021-06-01 17:38:38 +0200

    Enable unit selection and movement

--- Source/GDProject/GDProject.Build.cs
+++ Source/GDProject/GDProject.Build.cs
@@ -8,7 +8,7 @@ public class GDProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
